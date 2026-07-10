9ee8f05452a9558ded51173839666f6693268b90
arafffexe <ferozaamin52@gmail.com>
2026-07-11 05:14:39 +0600

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById("upload").addEventListener("change", function(e) {
+    const file = e.target.files[0];
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
 
+    img.onload = function() {
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+
+        canvas.width = img.width / 2;
+        canvas.height = img.height / 2;
+
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+});
